a43d8bde9e79810f2cf089db732082ec79527faf
Victoria Kalyniuk <tekalyniuk@hotmail.com>
2023-06-24 22:05:41 +0000

    added shortened url route

--- express_server.js
+++ express_server.js
@@ -2,10 +2,23 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+app.set("view engine", "ejs");
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.get("/urls/:id", (req, res) => {
+  const storedURL = "https://localhost:8080/urls"; // Replace with your actual long URL
+  const templateVars = { id: req.params.id, longURL: storedURL };
+  res.render("urls_show", templateVars);
+});
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
 
 app.get("/", (req, res) => {
   res.send("Hello!");
