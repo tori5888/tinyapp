83be8191a0ed98062d20b739b875598ddd6be9cd
Victoria Kalyniuk <tekalyniuk@hotmail.com>
2023-06-27 02:04:49 +0000

    add tests

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -19,6 +19,24 @@ describe('getUserByEmail', function() {
   it('should return a user with valid email', function() {
     const user = getUserByEmail("user@example.com", testUsers);
     const expectedUserID = "userRandomID";
-    assert.equal(user.id, expectedUserID);
+    assert.equal(user.id, expectedUserID, 'user ID should match the expected user ID');
+  });
+
+
+  it('should return null when an empty email is provided', function() {
+    const user = getUserByEmail("", testUsers);
+    assert.isNull(user, 'user should be null');
+  });
+
+  it('should return null when the users database is empty', function() {
+    const emptyUsers = {};
+    const user = getUserByEmail("user@example.com", emptyUsers);
+    assert.isNull(user, 'user should be null');
+  });
+
+  it('should return null when the users database is not provided', function() {
+    const user = getUserByEmail("user@example.com");
+    assert.isNull(user, 'user should be null');
   });
 });
+
